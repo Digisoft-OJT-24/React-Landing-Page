dd186f94fe1157ac49ab50ad1420e17816be104e
Okonutnut <darlitocabalse.acad@gmail.com>
2026-02-11 13:37:48 +0800

    hehey2

--- src/pages/home/components/feature-card.tsx
+++ src/pages/home/components/feature-card.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { cn } from "@/lib/utils";
-import { CenterOverlay } from "@/components/custom/center-overlay";
+// import { CenterOverlay } from "@/components/custom/center-overlay";
 
 export function FeaturesSectionDemo() {
   const features = [
